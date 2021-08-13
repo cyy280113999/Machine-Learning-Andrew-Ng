68a8bb99e5b43123ac4cc4de7a6a3c34861a3c15
cyy280113999 <53366958+cyy280113999@users.noreply.github.com>
2021-08-13 18:12:40 +0800

    添加注释

--- ex3_neural_network/ep12_train_nums.py
+++ ex3_neural_network/ep12_train_nums.py
@@ -1,4 +1,11 @@
-import numpy as np
+"""
+mnist手写数字数据集训练ep1模型测试
+
+模型：
+    ThisNet 继承了ep1中的 Net
+            改写了fit方法
+
+"""
 from ep1_nn import *
 import tensorflow.keras.datasets.mnist as mn
 
@@ -7,12 +14,13 @@ class ThisNet(Net):
     def fit(self, x, y, x_t, y_t, times=1000, eta=1e-1, pen=1e-1,
             batch_size=10,
             converge_gap=1e-10, diverge_gap=1e-2, acc_quit=99):
-
+        # 打印初始状态
         eva = self.evaluate(x, y, pen=pen)
         print('start:')
         print('eva', eva)
 
         lasterror = 1e5
+        # 记录每次学习的准确度和误差，用来画图
         tr_error = np.zeros((times,))
         tr_acc = np.zeros((times,))
         te_error = np.zeros((times,))
@@ -30,7 +38,7 @@ class ThisNet(Net):
             print('times:', time)
             print('eva', tr_eva)
             te_eva = self.evaluate(x_t, y_t, pen=pen)
-            tr_acc[time], tr_error[time] = tr_eva
+            tr_acc[time], tr_error[time] = tr_eva  # 记录每次学习的准确度和误差
             te_acc[time], te_error[time] = te_eva
             if acc_quit is None:
                 pass
@@ -44,6 +52,7 @@ class ThisNet(Net):
                 print(f'Diverged! by acc {eva[0]}')
                 break
             lasterror = eva[1]
+        # 绘制学习的准确度和误差曲线
         plt.figure()
         plt.subplot(1, 2, 1)
         plt.title('accuracy')
@@ -59,8 +68,10 @@ class ThisNet(Net):
         return tr_acc, tr_error, te_acc, te_error
 
 
+# 加载mnist数据集
 (tran_images, tran_labels), (test_images, test_labels) = mn.load_data()
 
+# 学习取1000个 测试取200个
 tran_images = tran_images[:1000]
 tran_images = tran_images / 255
 tran_labels = tran_labels[:1000]
@@ -69,12 +80,10 @@ test_images = test_images[:200]
 test_images = test_images / 255
 test_labels = test_labels[:200]
 
+# 网络输入为列向量，将图像矩阵平铺成向量
 length = 28 * 28
-
-
 def flattenImage(image):
     return image.reshape((length,))
-
 
 def reformatDataSet(dataSet):
     newSet = np.ndarray((length, dataSet.shape[0]))
@@ -82,11 +91,10 @@ def reformatDataSet(dataSet):
         newSet[:, i] = flattenImage(dataSet[i])
     return newSet
 
-
 tran_images_ = reformatDataSet(tran_images)
 test_images_ = reformatDataSet(test_images)
 
-
+# 网络输出为列向量，将图像类别标签转化成向量，只有一个元素为1
 def reformatLabel(labels):
     newLabels = np.ndarray((10, labels.shape[0]))
     for i in range(labels.shape[0]):
@@ -109,6 +117,7 @@ args = {
     'batch_size': 10,
     'acc_quit': 99.9
 }
+# 加载训练好的文件，不存在则重新训练
 try:
     weight = np.load('ep12_weight.npy', allow_pickle=True)
     model.wList = weight
@@ -124,9 +133,9 @@ except IOError as e:
 # bias & variance
 # 训练误差接近测试误差：underfit， 测试误差远大于训练误差： overfit
 # 教程是分别观察了 不同阶数多项式拟合 的误差，选取最优的多项式， 对于此网络可行吗？
-# e_tr = model.evaluate(tran_images_, tran_labels_, pen=1e-4)
-# e_te = model.evaluate(test_images_, test_labels_, pen=1e-4)
-# print(f'error of train: {e_tr}\n error of test: {e_te}')
+e_tr = model.evaluate(tran_images_, tran_labels_, pen=1e-4)
+e_te = model.evaluate(test_images_, test_labels_, pen=1e-4)
+print(f'error of train: {e_tr}\nerror of test: {e_te}')
 
 
 # def look_image(data):
